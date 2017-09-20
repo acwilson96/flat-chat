be1326e3b4a698a9f7a6b247b317eedafb351e3f
Alexander Wilson <acwilson96@gmail.com>
2017-09-20 21:28:06 +0100

    Import User and Model GraphQLObjects

--- schema.js
+++ schema.js
@@ -13,33 +13,17 @@ mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/flat-chat', {useMongoClient: true});
 
 /* Import Models */
-const User      = require('./models/user.js');
-const Message  = require('./models/message.js');
+const { User, UserType }        = require('./models/user.js');
+const { Message, MessageType }  = require('./models/message.js');
 
-// Message Type
-const MessageType = new GraphQLObjectType({
-    name: 'Message',
-    fields: () => ({
-        sender: { type: GraphQLString },
-        message: { type: GraphQLString },
-        timestamp: { type: GraphQLString }
-    })
-});
 
-// User Type
-const UserType = new GraphQLObjectType({
-    name: 'User',
-    fields: () => ({
-        username: { type: GraphQLString },
-        authToken: { type: GraphQLString },
-        registerDate: { type: GraphQLString }
-    }) 
-});
+
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        // Get a specific User.
         user: {
             type: UserType,
             args: {
@@ -49,12 +33,14 @@ const RootQuery = new GraphQLObjectType({
                 return User.findOne(args).then(user => user);
             }
         },
+        // Get all Users.
         users: {
             type: new GraphQLList(UserType),
             resolve(parentValue, args) {
                 return User.find().then(users => users);
             }
         },
+        // Get a specific Message.
         message: {
             type: MessageType,
             args: {
@@ -64,6 +50,7 @@ const RootQuery = new GraphQLObjectType({
                 return Message.find(args).then(message => message);
             }
         },
+        // Get all Messages.
         messages: {
             type: new GraphQLList(MessageType),
             resolve(parentValue, args) {
@@ -77,9 +64,11 @@ const RootQuery = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
     name: 'Mutation',
     fields: {
+        // Create a User Model.
         addUser: {
             type: UserType,
             args: {
+                // Requires a Username and Password field.
                 username: { type: new GraphQLNonNull(GraphQLString) },
                 password: { type: new GraphQLNonNull(GraphQLString) }
             },
@@ -93,9 +82,11 @@ const mutation = new GraphQLObjectType({
                 });
             }
         },
+        // Create a Message Model.
         addMessage: {
             type: MessageType,
             args: {
+                // Requires a message and an authToken (to ID a Sender).
                 authToken: { type: new GraphQLNonNull(GraphQLString) },
                 message: { type: new GraphQLNonNull(GraphQLString) }
             },
